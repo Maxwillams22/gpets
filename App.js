26ec847b457283af7a913e4cd759cad25a306bf9
Maxwillams22 <maxwillamssg@gmail.com>
2019-05-21 22:38:54 -0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import ListagemPet from './ListagemPet'
+import CadastroPet from './CadastroPet'
+
+export default class App extends React.Component {
+  render(){
+    return(
+      <CadastroPet />
+    )
+  }
+
+}
---
+++ ListagemPet.js
@@ -0,0 +1,47 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {Text, View, Button, StyleSheet } from 'react-native';
+
+
+export default class App extends React.Component {
+
+  state = {
+    gpets: []
+  }
+
+
+componentDidMount () {
+  fetch('https://pets-unipe.herokuapp.com/pets', { method: 'GET'})
+  .then(T => T.json())
+  .then(gpets => this.setState({ gpets }))
+}
+
+
+  render() {
+    const {gpets} = this.state
+    return (
+      <View>
+        <Text>Animais de Estimação</Text>
+        <Button title='Adicionar' onPress={() => console.log('criou o animal')} />
+        <View>
+          {gpets.map((gpet, key) => (
+            <View key={key}>
+              <Text>{gpet.name}o</Text>
+              <Text>{gpet.raca}o</Text>
+              <Button title='Editar' onPress={() => console.log('editou o animal')} />
+              <Button title='Excluir' onPress={() => console.log('excluiu o animal')} />
+              </View>
+          ))}
+        </View>
+      </View>
+    );
+  }
+}
+
